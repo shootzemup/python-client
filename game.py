4a54e9bee16c3d579ed9ccedbd67b5d26235728f
Romain G <rom1guyot@gmail.com>
2014-07-02 15:53:03 -0400

    Formatted comments for compatibility with the ooniki's comment generator

--- game.py
+++ game.py
@@ -12,17 +12,24 @@ from Graphx import graphx
 
 DONE = False
 
+#cl: Game: Manage the game loop
 class Game(object):
 	"""Handle the main loop of the game"""
 	def __init__(self):
 		super(Game, self).__init__()
 		logging.log(1, "Trace: Game.__init__()")
 		graphx.init()
+		#a: _init_time: allow to compute some running statistics
 		self._init_time = time.time()
+		#a: _rendering_time: contains the total time spent for rendering
 		self._rendering_time = 0
+		#a: _updating_time: contains the total time spent for updating
 		self._updating_time = 0
+		#a: _nb_updated: contains the total number of updates made by the game
 		self._nb_updates = 0
+		#a: _nb_renders: contains the total number of renders
 		self._nb_renders = 0
+		#a: _state_manager: allow to manage the state stack
 		self._state_manager = StateManager()
 
 	# Statistics
@@ -48,23 +55,23 @@ class Game(object):
 		return not self._nb_renders or self._rendering_time / self._nb_renders
 	def getAverageUpdatingTime(self):
 		return not self._nb_updates or self._updating_time / self._nb_updates
-	
-		
-	# Run the main game loop.
-	# It updates the game with a fixed time step but can update multiple time
-	# the game if it needs to catch up the lag introduce by rendering on a 
-	# slow hardware.
-	# See: [http://gameprogrammingpatterns.com/game-loop.html],
-	# [http://www.koonsolo.com/news/dewitters-gameloop/] for more info about
-	# how and why this game loop works well and avoid lots of synchronization
-	# problems.
+
+
+	#m: run: Run the main game loop.
+	#c: It updates the game with a fixed time step but can update multiple time
+	#c: the game if it needs to catch up the lag introduce by rendering on a
+	#c: slow hardware.
+	#c: See: [http://gameprogrammingpatterns.com/game-loop.html],
+	#c: [http://www.koonsolo.com/news/dewitters-gameloop/] for more info about
+	#c: how and why this game loop works well and avoid lots of synchronization
+	#c: problems.
 	def run (self):
 		logging.log(1, "Trace: Game.run()")
 		global DONE
 		previous = time.time()
 		lag = 0
 		while not DONE:
-			# compute the elapsed time since previous rendering
+			#c: compute the elapsed time since previous rendering
 			current = time.time()
 			elapsed = current - previous
 			previous = current
@@ -74,11 +81,11 @@ class Game(object):
 			# handle pressed keys
 			self.handlePressed()
 
-			# add the elapsed time to the lag. That allow to know whether 
-			# we need to update once or more on very slow hardware
+			#c: add the elapsed time to the lag. That allow to know whether
+			#c: we need to update once or more on very slow hardware
 			lag += elapsed
 
-			# catch up the lag
+			#c: catch up the lag
 			while lag >= conf['game_engine']['update_time_step']:
 				t = time.time()  # used for statistics
 				self.update()
@@ -92,40 +99,41 @@ class Game(object):
 			self._rendering_time += time.time() - t  # used for statistics
 			self._nb_renders += 1  # used for statistics
 
-	# handle a single event from the event queue
+	#m: handleEvents: handle a single event from the event queue
 	def handleEvents(self):
 		global DONE
 		logging.log(1, 'Trace: Game.handleEvent()')
 		for event in pygame.event.get():
 			logging.debug('Event: %s', event)
 			self._state_manager.handleEvent(event)
-			if event.type == pygame.QUIT: 
+			if event.type == pygame.QUIT:
 				DONE = True
 				time.sleep(10000)
 				sys.exit()
 			if event.type == pygame.KEYUP and event.key == pygame.K_ESCAPE:
 				DONE = True
 
-	# handle the keyboard and the mouse state
+	#m: handlePressed: handle the keyboard and the mouse state
 	def handlePressed(self):
 		logging.log(1, 'Trace: Game.handlePressed()')
 		kbs = pygame.key.get_pressed()
 		ms = pygame.mouse.get_pressed()
 		self._state_manager.handlePressed(kbs, ms)
 
-	# Update the game for one fixed-time step
+	#m: update: Update the game for one fixed-time step
 	def update(self):
 		logging.log(1, "Trace: ========> Game.update()")
 		self._state_manager.update()
 
-	# render the game objects.
-	# If some objects are moving from one position to another, it should
-	# linearly interpolate the real drawing position between the previous and 
-	# the current position of the object using the given interpolation factor.
-	# Note: to let the `render` step be as focused as possible on the actual 
-	# drawing, this interpolation computation should be done in the models of 
-	# the game objects.
+	#m: render: render the game objects.
+	#c: If some objects are moving from one position to another, it should
+	#c: linearly interpolate the real drawing position between the previous and
+	#c: the current position of the object using the given interpolation factor.
+	#c: Note: to let the `render` step be as focused as possible on the actual
+	#c: drawing, this interpolation computation should be done in the models of
+	#c: the game objects.
 	def render(self, interpolation):
+		#p: interpolation: float[0,1]: interpolation factor between current and next frame
 		logging.log(1, "Trace: ========> Game.render(%.5f)" % interpolation)
 		time.sleep(conf['game_engine']['simulate_hardware_lag']);
 		self._state_manager.render(interpolation)
